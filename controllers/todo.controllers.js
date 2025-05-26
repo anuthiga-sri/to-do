a93a56f03295b04d84ce30e21087cd7b8b58e24e
Anuthiga <anuthigasri@gmail.com>
2025-05-26 13:06:14 +0530

    - implemented todo history feature

--- controllers/todo.controllers.js
+++ controllers/todo.controllers.js
@@ -2,21 +2,21 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 exports.getTodoListOfAUser = async (req, res) => {
-    const userId = req.query.userId;
-    if (!userId) return res.status(400).json({ error: 'Missing userId' });
-  
-    try {
-      const todos = await prisma.todo.findMany({
-        where: { userId: Number(userId) }
-      });
-      res.status(200).json({ message: 'Successfully retrieved todo list.', todoList: todos });
-    } catch (err) {
-      res.status(500).json({ error: 'Failed to fetch todos' });
-    }
+  const userId = req.query.userId;
+  if (!userId) return res.status(400).json({ error: 'Missing userId' });
+
+  try {
+    const todos = await prisma.todo.findMany({
+      where: { userId: Number(userId) }
+    });
+    res.status(200).json({ message: 'Successfully retrieved todo list.', todoList: todos });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch todos' });
+  }
 };
 
 exports.addTodo = async (req, res) => {
-  const {title, description, dueDate, userId} = req.body
+  const { title, description, dueDate, userId } = req.body
   try {
     const todo = await prisma.todo.create({
       data: {
@@ -38,13 +38,36 @@ exports.updateTodo = async (req, res) => {
   const { todoId } = req.params;
   const { userId } = req.body;
   delete req.body.userId;
+  let oldStatus;
 
   try {
+
+    const allowedKeysForStatusUpdate = ['status', 'userId'];
+    const bodyKeys = Object.keys(req.body);
+
+    const isOnlyStatusChange = bodyKeys.every(key => allowedKeysForStatusUpdate.includes(key)) && bodyKeys.length === allowedKeysForStatusUpdate.length;
+
+    if (isOnlyStatusChange) {
+      const selectedTodo = await prisma.todo.findFirst({
+        where: { id: Number(todoId), userId: Number(userId) }
+      });
+      oldStatus = selectedTodo?.status ;
+    }
+
     const updatedTodo = await prisma.todo.update({
       where: { id: Number(todoId), userId: Number(userId) },
       data: { ...req.body }
     });
 
+    if (isOnlyStatusChange && oldStatus) {
+      await prisma.TodoHistory.create({
+        data: {
+          todoId: Number(todoId),
+          oldStatus,
+          newStatus: req.body.status
+        }
+      })
+    }
     res.status(200).json({ message: 'Todo updated successfully.', todo: updatedTodo });
   } catch (error) {
     console.log(error)
